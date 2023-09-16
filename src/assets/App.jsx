c3eac52d3abd43e398687d6d74449cd0f01d52d4
Utsav Kumar <utsavkumarapp@gmail.com>
2023-09-16 22:28:24 +0530

    first commit

---
+++ src/assets/App.jsx
@@ -0,0 +1,12 @@
+import SlickCarousel from "./SlickCarousel";
+import Carousel from "./Carousel";
+
+const App = () => {
+  return (
+    <main>
+      {/* <Carousel /> */}
+      <SlickCarousel />
+    </main>
+  );
+};
+export default App;
